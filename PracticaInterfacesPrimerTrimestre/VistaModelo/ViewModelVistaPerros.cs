e43a431b0079f4f87849165508842f00d0c243ce
Diego Cuesta <dcuestaprieto@gmail.com>
2024-01-21 18:06:05 +0100

    now VistaPerros shows all the dogs from the API

---
+++ PracticaInterfacesPrimerTrimestre/VistaModelo/ViewModelVistaPerros.cs
@@ -0,0 +1,18 @@
+﻿using API.Modelo;
+using PracticaInterfacesPrimerTrimestre.Modelo;
+using PracticaInterfacesPrimerTrimestre.Vista;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaInterfacesPrimerTrimestre.VistaModelo
+{
+    public class ViewModelVistaPerros
+    {
+        //public ObservableCollection<Dog> ListaPerros { get; set; } = VistaInicio.Dogs;
+        //public ObservableCollection<Dog> ListaPerros { get; set; } = VariablesCompartidas.Dogs;
+}
+}
