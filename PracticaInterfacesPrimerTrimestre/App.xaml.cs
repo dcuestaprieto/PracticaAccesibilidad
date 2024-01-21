f3e4726583af5f9e97bc410491bb9f026f16000a
Diego Cuesta <dcuestaprieto@gmail.com>
2024-01-22 00:17:25 +0100

    change app config

--- PracticaInterfacesPrimerTrimestre/App.xaml.cs
+++ PracticaInterfacesPrimerTrimestre/App.xaml.cs
@@ -1,19 +1,24 @@
-﻿using PracticaInterfacesPrimerTrimestre.Repositorios;
+﻿using PracticaInterfacesPrimerTrimestre.Modelo;
+using PracticaInterfacesPrimerTrimestre.Repositorios;
 namespace PracticaInterfacesPrimerTrimestre;
 
 public partial class App : Application
 {
 	public static UsuarioRepositorio UsuarioRepositorio { get; set; }
+    public static FavoritosRepositorio FavoritosRepositorio { get; set; }
 
-    public App(UsuarioRepositorio usuarioRepositorio)
+    public App(UsuarioRepositorio usuarioRepositorio, FavoritosRepositorio favoritos)
 	{
-		UsuarioRepositorio = usuarioRepositorio;
-		InitializeComponent();
-
-        UsuarioRepositorio = usuarioRepositorio;
         InitializeComponent();
+        UsuarioRepositorio = usuarioRepositorio;
+        FavoritosRepositorio = favoritos;
         MainPage = new AppShell();
 	}
 
-
+    private async void CerrarSesion(object sender, EventArgs e)
+    {
+        //Y borrar el usuario actual del fichero VariablesCompartidas
+        VariablesCompartidas.CurrentUser = null;
+        await AppShell.Current.GoToAsync(nameof(Vista.VistaInicio));
+    }
 }
