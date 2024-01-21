ff4f637c9b348561297b623ac2709157b81e45ce
Diego Cuesta <dcuestaprieto@gmail.com>
2024-01-22 00:14:32 +0100

    created the FavoritosRepositorio

---
+++ PracticaInterfacesPrimerTrimestre/Repositorios/FavoritosRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using PracticaInterfacesPrimerTrimestre.Modelo;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaInterfacesPrimerTrimestre.Repositorios
+{
+    public class FavoritosRepositorio
+    {
+        private readonly string route;
+        private readonly SQLiteConnection connection;
+        private const string tableName = "favoritos";
+
+        public FavoritosRepositorio(string route)
+        {
+            this.route = route;
+            this.connection = new SQLiteConnection(this.route);
+            System.Diagnostics.Debug.WriteLine($"\nRuta de Base de datos: {this.route}\n");
+            if (!connection.TableMappings.Any(e => e.MappedType.Name == tableName))
+            {
+                connection.CreateTable<Favorito>();
+            }
+        }
+
+        internal static ObservableCollection<Dog> FetchFavouritesDog(string v)
+        {
+            return null;
+        }
+    }
+}
